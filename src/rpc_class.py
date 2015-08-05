9b7282d11f3755603c04139d6236bb60c7bddd38
Mike M <marzig76@gmail.com>
2015-08-04 21:47:57 -0400

    use new rpc class with shift_example

--- src/rpc_class.py
+++ src/rpc_class.py
@@ -20,13 +20,12 @@ class coinrpc:
                 self.rpcpass = line[1].strip()
 
     def rpccon(self):
-        rpcconnection = "http://" + self.rpcuser + ":" + self.rpcpass + "@127.0.0.1:8332"
+        rpcconnection = "http://" + self.rpcuser + ":" + self.rpcpass + "@127.0.0.1:18332"
         rpc = AuthServiceProxy(rpcconnection)
         return rpc
 
 
 # test it
-mycoin = coinrpc("../../bitcoin-0.10.2/bitcoin/bitcoin.conf")
-bitcoinrpc = mycoin.rpccon()
+bitcoinrpc = coinrpc("../../bitcoin-0.10.2/bitcoin/bitcoin.conf").rpccon()
 
 print bitcoinrpc.getinfo()
--- src/shift_example.py
+++ src/shift_example.py
@@ -1,38 +1,17 @@
 import re
 import json
+from rpc_class import coinrpc
 from restful_lib import Connection
 from bitcoinrpc.authproxy import AuthServiceProxy
 
 # get bitcoin rpc config
-conffile = open("../../bitcoin-0.10.2/bitcoin/bitcoin.conf")
-
-for lines in conffile:
-    line = re.split("=", lines)
-    if line[0] == "rpcuser":
-        rpcuser = line[1].strip()
-    if line[0] == "rpcpassword":
-        rpcpass = line[1].strip()
-
-# create RPC connection
-bitcoinrpcconnection = "http://" + rpcuser + ":" + rpcpass + "@127.0.0.1:8332"
-bitcoin_rpc = AuthServiceProxy(bitcoinrpcconnection)
+bitcoinrpc = coinrpc("../../bitcoin-0.10.2/bitcoin/bitcoin.conf").rpccon()
 
 # get litecoin rpc config
-conffile = open("~/.litecoin/.bitcoin.conf")
-
-for lines in conffile:
-    line = re.split("=", lines)
-    if line[0] == "rpcuser":
-        rpcuser = line[1].strip()
-    if line[0] == "rpcpassword":
-        rpcpass = line[1].strip()
-
-# create RPC connection
-litecoinrpcconnection = "http://" + rpcuser + ":" + rpcpass + "@127.0.0.1:8332"
-litecoin_rpc = AuthServiceProxy(litecoinrpcconnection)
+litecoinrpc = coinrpc("../../litcoin-xxx/bitcoin/bitcoin.conf").rpccon()
 
 # make a new bitcoin address to receive the shifted funds
-newaddress = bitcoin_rpc.getnewaddress()
+newaddress = bitcoinrpc.getnewaddress()
 
 # connect to shapeshift API
 base_url = "https://shapeshift.io"
@@ -47,8 +26,8 @@ for item in btc_shift:
         response = json.loads(btc_shift[item])
 
 for key, value in response.iteritems():
-    if key = "deposit":
+    if key == "deposit":
         depositaddr = value
 
 # send to deposit address
-litecoin_rpc.sendtoaddress(depositaddr, 10)
+litecoinrpc.sendtoaddress(depositaddr, 10)
