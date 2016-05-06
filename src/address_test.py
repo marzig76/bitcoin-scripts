76b290b41ce598604f7db37c30c4d66f8686f31a
Mike Marzigliano <marzig76@gmail.com>
2016-05-06 18:54:55 -0400

    testing pubkey to address conversions

--- src/address_test.py
+++ src/address_test.py
@@ -1,6 +1,7 @@
 from block import block
 from script import script
-import sqlite3
+from blockutil import pkhash2addr
+from blockutil import pubkey2addr
 
 blockfile = '/home/marzig76/.bitcoin/blocks/blk00004.dat'
 blockstream = open(blockfile, 'rb')
@@ -8,7 +9,11 @@ b = block(blockstream)
 
 for t in b.txs:
 
-  for ti in t.tx_inputs:
-    ti.sigscript.encode('hex')
+    for ti in t.tx_inputs:
+        print 'in addr: ' + pubkey2addr(ti.sigscript)
 
-  for to in t.tx_outputs:
+    for to in t.tx_outputs:
+        spk = to.script_pk.encode('hex')
+
+        if spk[:6] == '76a914':
+            print 'out addr: ' + pkhash2addr(spk[6:46])
